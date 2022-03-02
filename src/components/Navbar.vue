fa066a335cfc799721210aac29d99a22ba78d1e6
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-03-02 18:49:41 +0700

    feat(navbar): add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar fixed color="white" elevate-on-scroll>
+    <v-spacer></v-spacer>
+    <v-img max-width="100" src="../assets/logo.png"></v-img>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
