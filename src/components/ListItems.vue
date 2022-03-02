18f9375b2ef8937898451344ad7f0c8a9696ee4a
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-03-02 23:32:19 +0700

    feat(listItems): add list item news

---
+++ src/components/ListItems.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-col cols="12" class="mb-10">
+      <v-card flat>
+        <v-row>
+          <v-col cols="5">
+            <v-img
+              class="rounded-xl ma-auto"
+              src="https://kotak.modoc.co.id/news/headline_20220224101807.jpg"
+              lazy-src="https://picsum.photos/id/11/100/60"
+              height="200"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+          <v-col>
+            <v-list two-line>
+              <v-list-item>
+                <div class="pr-3">
+                  <v-badge
+                    offset-y="18"
+                    bordered
+                    color="light-blue lighten-1"
+                    icon="mdi-check-bold"
+                    overlap
+                  >
+                    <v-avatar size="40">
+                      <v-img
+                        src="https://kotak.modoc.co.id/images/profile/60b6e88193477.png"
+                      ></v-img>
+                    </v-avatar>
+                  </v-badge>
+                </div>
+
+                <v-list-item-content>
+                  <v-list-item-title class="text-caption">
+                    Modoc News Official
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="text-caption text-secondary">
+                    20 jam yang lalu
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+                temporibus dolorem, corrupti dolor fugiat delectus consectetur
+                quod suscipit tenetur illo?
+              </p>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItems",
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grey lighten-4">
+    <Navbar />
+    <v-container class="my-16">
+      <article class="white pa-5 mt-10 rounded-lg">
+        <p class="text-h6">Lembar Berita</p>
+        <v-row>
+          <ListItems v-for="i in 5" :key="i" />
+        </v-row>
+      </article>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import ListItems from "../components/ListItems.vue";
+import Navbar from "../components/Navbar.vue";
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    ListItems,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
