5b47d04fdf348b13788d00bc0aed53fea2a4e282
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-03-04 00:37:38 +0700

    build(axios): add axios and add page blog

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import "./assets/css/style.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, { $axios: axios });
 
 new Vue({
   router,
---
+++ src/views/Blog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <b>Page Article - {{ this.$route.params.title }} </b> Lorem ipsum dolor sit
+    amet consectetur adipisicing elit. Doloribus unde dolores illo possimus sed
+    iure perspiciatis in laboriosam magni qui. Placeat deleniti reprehenderit
+    earum, autem fugiat consequuntur accusamus nulla debitis illo doloribus
+    molestiae qui cumque, impedit repellat, quisquam quae! Corporis, ut quia!
+    Quibusdam obcaecati ipsa dicta ullam provident eius nam officia et sit
+    blanditiis maiores corporis dolorum, sint dolores recusandae error pariatur
+    aliquid voluptate magni impedit quae amet ad id earum. Vitae, omnis!
+    Exercitationem voluptas voluptates repellendus dolor quisquam, officia quod
+    consequatur quos! Ullam esse debitis est. Beatae dolore nesciunt molestiae
+    ad, cumque magnam repellat iure atque asperiores, sunt itaque?
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
